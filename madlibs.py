e8f6455ac892e7a17e642b23126fea2a4cd43535
Cristian Montes <cmontesmujica@gmail.com>
2022-04-17 11:54:06 -0600

    added 2 extea madlibs to make a random madlib

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,12 @@
+from traceback import print_tb
+
+
 adj = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 famous_person = input("Famous person: ")
 
-madlib = f" I love tcos becasue they are{adj}! Every time I eat them they make so {verb1} \
+madlib = f" I love tcos becasue they are {adj}! Every time I eat them they make so {verb1} \
 I want {verb2} every time I think about tacos. Even{famous_person} loves them and they are a super fit person"
+
+print(madlib)

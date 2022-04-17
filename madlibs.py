3468851147bf661f5949e1bf5eefa66fd63a017a
Cristian Montes <cmontesmujica@gmail.com>
2022-04-16 23:49:59 -0600

    working on mad libs project

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f" I love tcos becasue they are{adj}! Every time I eat them they make so {verb1} \
+I want {verb2} every time I think about tacos. Even{famous_person} loves them and they are a super fit person"

76ebee5fd1cca531ad5f8f796d173f27bc4e24e2
Cristian Montes <cmontesmujica@gmail.com>
2022-04-18 21:23:32 -0600

    added the logic for the computer to guess our number

--- guess_random_number.py
+++ guess_random_number.py
@@ -13,4 +13,28 @@ def guessNumber(x:int):
     print(f'Congrats, you have guessed the random number {x}')
 
 
-guessNumber(10)
+# guessNumber(10)
+
+
+# THIS MAKES THE COMPUTER GUESS THE CORRECT NUMBER
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+           guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)').lower()
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f'Yay! The number computer guessed is correct, {guess}')
+
+computer_guess(10)

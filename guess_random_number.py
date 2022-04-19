fd59e30b56ea14235102069f9ad21fadd93aba4b
Cristian Montes <cmontesmujica@gmail.com>
2022-04-18 21:00:22 -0600

    added guess number function to guess_random_number

--- guess_random_number.py
+++ guess_random_number.py
@@ -3,4 +3,14 @@ import random
 def guessNumber(x:int):
     random_number = random.randint(1, x)
     guess = 0
-    while guess != random_number
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}'))
+        if guess < random_number:
+            print('Sorry, guess again. Too Low')
+        elif guess > random_number:
+            print('Sorry, guess again. Too High,')
+    
+    print(f'Congrats, you have guessed the random number {x}')
+
+
+guessNumber(10)
